6448f613ceb3c02670e455f3bf5f25f492822991
AmirSaudagar55 <saudagaramir55@gmail.com>
2024-06-12 05:42:06 +0530

    D04 : Other way to create server using ExpressJS

--- D04_ExpressJS/index.js
+++ D04_ExpressJS/index.js
@@ -6,11 +6,38 @@
 //3)Simplified middleware (traditional : manual calling in route handler, expressJS : Can be applied to specific route)
 //4)Provide more abstraction 
 
+const http = require('http')
 const express = require('express')
-
 const app = express()
 
-app.listen(8000, ()=>{
-    console.log("Server is listening on port 8000")
+app.get('/',(req,res)=>{
+    console.log("request received")
+    res.end("welcome to home page")
 })
+
+const myServer = http.createServer(app)
+
+let portNo = 8000  
+
+myServer.listen(portNo, ()=>{
+    console.log(`Server is listening on Port : ${portNo}`)
+})
+
+
+
+
+// const express = require('express')
+
+// const app = express()
+
+// app.get('/',(req, res)=>{
+//     console.log("Request received")
+//     res.end("Welcome to Home Page")
+// })
+
+// app.listen(8000, ()=>{
+//     console.log("Server is listening on port 8000")
+// })
+
+
 
