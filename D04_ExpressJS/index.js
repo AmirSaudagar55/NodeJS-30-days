8a8b4f148bac6e81e4ea361f5438f630c667f566
AmirSaudagar55 <saudagaramir55@gmail.com>
2024-06-12 05:44:34 +0530

    D04 : Adding more paths using ExpressJS

--- D04_ExpressJS/index.js
+++ D04_ExpressJS/index.js
@@ -6,38 +6,43 @@
 //3)Simplified middleware (traditional : manual calling in route handler, expressJS : Can be applied to specific route)
 //4)Provide more abstraction 
 
-const http = require('http')
-const express = require('express')
-const app = express()
-
-app.get('/',(req,res)=>{
-    console.log("request received")
-    res.end("welcome to home page")
-})
-
-const myServer = http.createServer(app)
-
-let portNo = 8000  
-
-myServer.listen(portNo, ()=>{
-    console.log(`Server is listening on Port : ${portNo}`)
-})
-
-
-
-
+// const http = require('http')
 // const express = require('express')
-
 // const app = express()
 
-// app.get('/',(req, res)=>{
-//     console.log("Request received")
-//     res.end("Welcome to Home Page")
+// app.get('/',(req,res)=>{
+//     console.log("request received")
+//     res.end("welcome to home page")
 // })
 
-// app.listen(8000, ()=>{
-//     console.log("Server is listening on port 8000")
+// const myServer = http.createServer(app)
+
+// let portNo = 8000  
+
+// myServer.listen(portNo, ()=>{
+//     console.log(`Server is listening on Port : ${portNo}`)
 // })
+
+
+
+
+const express = require('express')
+
+const app = express()
+
+app.get('/',(req, res)=>{
+    console.log("Request received")
+    res.end("Welcome to Home Page")
+})
+
+app.get('/about',(req, res)=>{
+    console.log("Request for About us page")
+    res.end("About us page")
+})
+
+app.listen(8000, ()=>{
+    console.log("Server is listening on port 8000")
+})
 
 
 
