78738bce6ebc13c81498d3bc4ee89f83568012ec
AmirSaudagar55 <saudagaramir55@gmail.com>
2024-06-16 08:32:13 +0530

    D08 : Postman Handling requests

--- D07_A_REST_API/index.js
+++ D07_A_REST_API/index.js
@@ -7,10 +7,17 @@
 // PATCH /users/1 - Edit the user with ID 1
 // DELETE /users/1 - Delete the user with ID 1
 
+const { urlencoded } = require('express')
 const express = require('express')
+const fs = require('fs')
+const _ = require('lodash')
 const data = require("./MOCK_DATA.json")
 
 const app = express()
+
+
+app.use(express.urlencoded({ extended: false }))
+
 
 app.get('/users',(req, res)=>{
     const users = data
@@ -26,16 +33,6 @@ app.get('/users',(req, res)=>{
 
     res.send(html)
 })
-.post((req,res)=>{              //handle different requests on same route
-    res.send("Pending")
-})
-.patch((req,res)=>{             
-    res.send("Pending")
-})
-.delete((req,res)=>{              
-    res.send("Pending")
-})
-
 
 
 app.get(`/users/:id`, (req, res)=>{             //to specify dynamic parameter we use ':variable_name'
@@ -45,8 +42,42 @@ app.get(`/users/:id`, (req, res)=>{             //to specify dynamic parameter w
     res.send(html)
 })
 
+// ---------------------------------------------------------------------------------------------
+
 app.get(`/api/users`, (req, res)=>{
     res.json(data)
+})
+
+app.post('/api/users', (req, res)=>{
+
+    const body = req.body;
+    const newUser = {id : (data.length)+1, ...body}
+
+    // if(data.find(user => user === JSON.stringify(newUser))){      // https://www.freecodecamp.org/news/javascript-comparison-operators-how-to-compare-objects-for-equality-in-js/#:~:text=How%20to%20Compare%20Objects%20Using%20The%20JSON.stringify()%20Function%20in%20JavaScript
+    //     console.log("USER FOUND ")
+    // }
+
+
+    if(data.find((user) => {
+        let omitUser1 = _.omit(user, 'id')
+        let omitUser2 = _.omit(newUser, 'id')
+        if(_.isEqual(omitUser1, omitUser2))
+        {
+            return true;
+        }
+    }))
+    {
+        console.log("user found")
+    }
+
+    
+    data.push(newUser)
+
+    fs.writeFile("./MOCK_DATA.json",JSON.stringify(data), (err, data)=>{
+        if(err) console.log(err)
+        else res.end(`Successful\n ID : ${newUser.id}`)
+    })
+
 })
 
 app.get(`/api/users/:id`, (req, res)=>{
@@ -54,6 +85,9 @@ app.get(`/api/users/:id`, (req, res)=>{
     const user = data.find((user) => user.id==id)
     res.json(user)
 })
+
+
+
 
 app.listen(8000, ()=>{
     console.log("Server is listening on PORT 8000")
