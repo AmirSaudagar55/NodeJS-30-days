4641fe85c518d86b9aa551bc5f3befec9e5d09e2
AmirSaudagar55 <saudagaramir55@gmail.com>
2024-06-23 07:44:14 +0530

    D15 : Server Side Rendering Using EJS

---
+++ D15_EJS/Controllers/url.js
@@ -0,0 +1,52 @@
+const URL = require('../Model/Schema.js')
+const shortid  = require('shortid')
+
+async function handleCreateShortURL(req, res){
+
+    if(!req.body.url){
+        res.status(400).json({message : "Provide Proper URL"});
+    }
+    const urls = await URL.find({})
+    const givenURL = req.body.url
+    const id = shortid.generate()
+
+    const shortURL = await URL.create({
+        shortID : id,
+        originalURL : givenURL
+    })
+
+    res.status(201).render("home",{id : shortURL.shortID, doc : shortURL, urls : urls})
+
+}
+
+async function handleGetURLs (req, res){
+    const id = req.params.shortID
+    const redirectURL= await URL.findOneAndUpdate({shortID : id}, 
+        {
+            $push : {visitHistory : {timestamps : Date.now()} }
+        }
+    );
+    if(!redirectURL)   {
+        res.status(500).json({message : "No URL exists."})
+    } 
+    else
+    {
+        res.status(200).redirect(redirectURL.originalURL);
+    }
+}
+
+async function handleShowAnalytics(req, res){
+    const id = req.params.shortID
+    const foundDoc = await URL.findOne({shortID : id});
+    if(!foundDoc){
+        res.status(500).json({message : "URL not found in database. Try Entering correct URL."})
+    }
+    const length = foundDoc.visitHistory.length;
+    res.status(200).json({totalClicks : length, analytics : foundDoc});
+}
+
+module.exports = {
+    handleCreateShortURL,
+    handleGetURLs,
+    handleShowAnalytics
+}
