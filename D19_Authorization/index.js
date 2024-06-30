269f86282070b3704353009ecc614ff5cbac6c8e
AmirSaudagar55 <saudagaramir55@gmail.com>
2024-06-30 08:50:37 +0530

    D19 : Bcrypt password hashing

--- D19_Authorization/index.js
+++ D19_Authorization/index.js
@@ -1,8 +1,22 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
 const app = express()
 const authPage = require("./middlewares")
 
 app.use(express.json())
+
+
+app.post("/user", async (req, res)=>{
+    const user = req.body
+    const salt = await bcrypt.genSalt(10);
+    const password = user.password;
+    const hashedPassword = await bcrypt.hash(password,salt);
+    console.log(hashedPassword);
+    console.log(await bcrypt.compare(password,hashedPassword))
+    user.password = hashedPassword;
+    res.status(201).json({"message" : "User Created Successfully", user})
+})
+
 
 app.get("/marks",authPage(["teacher", "admin"]),(req, res)=>{
     res.status(200).json({
@@ -11,7 +25,6 @@ app.get("/marks",authPage(["teacher", "admin"]),(req, res)=>{
         "Tony" : 99
     })
 })
-
 
 app.listen(8000, ()=>{
     console.log("Server is listening on port 8000")
